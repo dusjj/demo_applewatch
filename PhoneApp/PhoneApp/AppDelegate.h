6303a183a39680b04056623c1520e99508ded5cf
Du Shaojiang <dusj@mac-dusj.local>
2017-06-06 13:56:15 +0800

    first commit.

---
+++ PhoneApp/PhoneApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhoneApp
+//
+//  Created by Du Shaojiang on 2017/6/6.
+//  Copyright © 2017年 Du Shaojiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhoneApp/PhoneApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhoneApp
+//
+//  Created by Du Shaojiang on 2017/6/6.
+//  Copyright © 2017年 Du Shaojiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
